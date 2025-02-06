f93aa34f99aebbf0a4e3b2c77db1a6c8285671c6
ausudu <zokon753639@gmail.com>
2025-02-06 19:03:42 +0900

    Update tur_cmd_vel.py

--- rosws/src/military_ai/military_ai/AMR_sprint/tur_cmd_vel.py
+++ rosws/src/military_ai/military_ai/AMR_sprint/tur_cmd_vel.py
@@ -23,17 +23,17 @@ class ControlCMDModeNode(Node):
         cmd_vel_msg = Twist()
 
         if msg.data == 0.0:  # 좌회전
-            cmd_vel_msg.angular.z = 0.5
-            cmd_vel_msg.linear.x = 0.1
+            cmd_vel_msg.angular.z = 0.25
+            cmd_vel_msg.linear.x = 0.05
             self.get_logger().info("Turning Left")
         
         elif msg.data == 1.0:  # 직진
-            cmd_vel_msg.linear.x = 0.2
+            cmd_vel_msg.linear.x = 0.1
             self.get_logger().info("Moving Forward")
         
         elif msg.data == 2.0:  # 우회전
-            cmd_vel_msg.angular.z = -0.5
-            cmd_vel_msg.linear.x = 0.1
+            cmd_vel_msg.angular.z = -0.25
+            cmd_vel_msg.linear.x = 0.05
             self.get_logger().info("Turning Right")
            
         elif msg.data == 3.0:  # 정지
